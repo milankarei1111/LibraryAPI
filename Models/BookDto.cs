b0db110cf45b1aaa67fbbd9e977400f0804bb8fc
wanru_tsai <milankarei1111@gmail.com>
2021-04-13 23:16:04 +0800

    git init and add Models

---
+++ Models/BookDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Models
+{
+    public class BookDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Descripction { get; set; }
+        public int Pages { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
