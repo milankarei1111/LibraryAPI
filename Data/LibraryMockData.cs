3bb2b6a9bbffa97d44b9074d64dbd9d04172e4ca
wanru_tsai <milankarei1111@gmail.com>
2021-04-13 23:33:27 +0800

    建立內存資料

---
+++ Data/LibraryMockData.cs
@@ -0,0 +1,73 @@
+﻿using LibraryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Data
+{
+    public class LibraryMockData
+    {
+        //取得 LibraryMockData 實例
+        public static LibraryMockData Current { get; } = new LibraryMockData();
+        public List<AuthorDto> Authors { get; set; }
+        public List<BookDto> Books { get; set; }
+
+        public LibraryMockData()
+        {
+            Authors = new List<AuthorDto>
+            {
+                new AuthorDto
+                {
+                    Id = new Guid("72D5B5F5-3008-49B7-B0D6-CC337F1A330"),
+                    Name = "Author 1",
+                    Age = 46,
+                    Email = "author1@xxx.com",
+                },
+                new AuthorDto
+                {
+                    Id = new Guid("7D04A48E-BE4E-468E-8CE2-3AC0C79549"),
+                    Name = "Author 2",
+                    Age = 38,
+                    Email = "author2@xxx.com",
+                }
+            };
+
+            Books = new List<BookDto>
+            {
+                new BookDto
+                {
+                    Id = new Guid("7D8EBDA9-2634-4C0F-9469-0695D6132110"),
+                    Title = "Book 1",
+                    Description = "Description of Book 1",
+                    Pages = 281,
+                    AuthorId = new Guid("72D5B5F5-3008-49B7-B0D6-CC337F1A330"),
+                },
+                new BookDto
+                {
+                    Id = new Guid("7D8EBDA9-2634-4C0F-9469-0695D6132120"),
+                    Title = "Book 2",
+                    Description = "Description of Book 2",
+                    Pages = 370,
+                    AuthorId = new Guid("72D5B5F5-3008-49B7-B0D6-CC337F1A330"),
+                },
+                new BookDto
+                {
+                    Id = new Guid("7D8EBDA9-2634-4C0F-9469-0695D6132130"),
+                    Title = "Book 3",
+                    Description = "Description of Book 3",
+                    Pages = 229,
+                    AuthorId = new Guid("7D04A48E-BE4E-468E-8CE2-3AC0C79549"),
+                },
+                new BookDto
+                {
+                    Id = new Guid("7D8EBDA9-2634-4C0F-9469-0695D6132140"),
+                    Title = "Book 4",
+                    Description = "Description of Book 4",
+                    Pages = 440,
+                    AuthorId = new Guid("7D04A48E-BE4E-468E-8CE2-3AC0C79549"),
+                }
+            };
+        }
+    }
+}
