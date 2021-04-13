dbf76c5bd4b1ed0e6b30d012701433b39ec3b556
wanru_tsai <milankarei1111@gmail.com>
2021-04-13 23:33:41 +0800

    Modify 參數名稱

--- Models/BookDto.cs
+++ Models/BookDto.cs
@@ -9,7 +9,7 @@ namespace LibraryAPI.Models
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Descripction { get; set; }
+        public string Description { get; set; }
         public int Pages { get; set; }
         public Guid AuthorId { get; set; }
     }
